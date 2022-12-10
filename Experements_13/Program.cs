5a97813b787eea7f419870313187b8f9c4eae426
Sergey Shalagin <maloi2000_1@mail.ru>
2022-12-10 16:38:43 +0300

    Задача 27

--- Experements_13/Program.cs
+++ Experements_13/Program.cs
@@ -7,11 +7,15 @@
 Console.Clear();
 Console.WriteLine("Введите число: ");
 int i = int.Parse(Console.ReadLine()!);
-int sum = 0;
-while (i > 0)
+int GetSumNums(int number)
 {
-    int num = i % 10;
-    i = i / 10;
-    sum = sum + num;
+    int sum = 0;
+    while (i > 0)
+    {
+        int num = i % 10;
+        i = i / 10;
+        sum = sum + num;
+    }
+    return sum;
 }
-Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+Console.WriteLine($"Сумма цифр = {GetSumNums(i)}");
